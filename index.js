eb9a5b478a727644683d4b41b40dcf10175337c9
Abdulloh <qiyomovabdulloh3@gmail.com>
2021-06-18 16:00:34 +0500

    js-hm

---
+++ index.js
@@ -0,0 +1,118 @@
+// ABDULLOH'S HOMEWORK
+
+// Begin-1 P-?
+
+// var P = 4 
+//     a = 10
+// console.log(P*a)
+
+
+
+// Begin-2 S-?
+
+// var S = 4
+// console.log(S*2)
+
+
+
+// Begin-3 S-? P-?
+
+// var a = 5
+//     b = 7
+// console.log(a*b)
+// console.log(2*(a+b))
+
+
+
+// Begin-4 L-?
+
+// var pi = 3.14
+//     d = 2
+// console.log(pi*d)
+
+
+
+// Begin-5 V-? S-?
+
+// var a = 3
+// console.log(a**3)
+// console.log(6*a**2)
+
+
+
+// Begin-6 V-? S-?
+
+// var a = 2
+//     b = 3
+//     c = 4
+// console.log(a*b*c)
+// console.log(2*(a*b+b*c+a*c))
+
+
+
+// Begin-7 L-? S-?
+
+// var pi = 3.14
+//     R = 5
+// console.log(2*pi*R)
+// console.log(pi*R**2)
+
+
+
+// Begin-8 
+
+// var a = 5
+//     b = 7
+// console.log((a+b)/2)
+
+
+// Begin-9 Ildiz
+
+// var a = 5
+//     b = 5
+//  console.log((a*b)**0.5)
+
+
+// Begin-10
+
+// var a = 5
+//     b = 6
+// console.log(a*b, a+b, a**2, b**2)
+
+
+// Begin-11
+
+// var a = 4
+//     b = 7
+// console.log(a+b, a*b, a, b)
+
+
+// Begin-12
+
+// var a = 3
+//     b = 4
+//     c = ((a**2+b**2)**0.5)
+// console.log((a**2+b**2)**0.5)
+// console.log(a+b+c)
+
+
+// Begin-13
+
+// var R1 = 4
+//     R2 = 3
+//     pi = 3.14
+//     S1 = (pi*R1)
+//     S2 = (pi*R2)
+//     S3 = (pi*(R1-R2))
+// console.log(S1, S2, S3)
+const pi = 3.14
+
+
+// Begin-15
+// var S = 5
+// var R = 2*((2*pi)/S)
+// var L = pi*R**2
+// console.log( L,S,)
+
+
+
