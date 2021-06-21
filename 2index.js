7fd7f8e4c66aeb822cdeed60b42c4165593b11bc
Abdulloh <qiyomovabdulloh3@gmail.com>
2021-06-21 14:11:05 +0500

    abd-js

---
+++ 2index.js
@@ -0,0 +1,98 @@
+// 13
+const p = 3.14
+// var R1 = 3
+//     R2 = 2
+//     S1 = p*R1
+//     S2 = p*R2
+//     S3 = p*(R1-R2)
+// console.log(S1, S2, S3)
+
+// 14
+// var L = 5
+//     r = L/(2*p)
+//     S = p*r**2
+// console.log(S)
+
+// 15
+// var S = 5
+//     r = S/(2*p)
+//     d = p*r**2
+// console.log(d)
+
+// 16
+// var X2 = 3
+//     X1 = 5
+// console.log(Math.abs(X2-X1))
+
+// 17
+// var A = 2
+//     B = 3
+//     C = 4
+//     AC = A+C
+//     BC = B+C
+// console.log(AC, BC)
+
+// 18
+// var A = 4
+//     C = 6
+//     B = 9
+//     AC = A*C
+//     BC = B*C
+// console.log(AC, BC)
+
+// 19
+// var a = 4
+//     b = 5
+//     P = 2*(a+b)
+//     S = a*b
+// console.log(P, S)
+
+// 20
+// var x1 = 6
+//     y1 = 5
+//     x2 = 4
+//     y2 = 3
+// console.log(Math.sqrt(Math.pow((x1-x2),2)+Math.pow((y1-y2),2)))
+
+// 21
+// var a = 3
+//     b = 4
+//     c = 5
+//     P = (a+b+c)/2
+//     S = Math.sqrt(P*(P-a)*(P-b)*(P-c))
+// console.log(S)
+
+// 22
+// var a = 10
+// var b = 20
+// var c = a //10
+// var a = b //20
+// var b = c //10
+// console.log(a,b)
+
+// 23
+// var a = 7
+//     b = 8
+//     c = 9
+// var d = a
+//     a = b
+//     b = c
+//     c = d
+// console.log(a,b,c)
+
+
+
+
+
+
+
+// console.log(Math.sqrt(25))
+// console.log(Math.round(8.3, 8,9))
+// console.log(Math.round(8.5))
+// console.log(Math.abs(-11))
+// console.log(Math.ceil(4.2))
+// console.log(Math.ceil(4.000000001))
+// console.log(Math.floor(4.99999))
+// console.log(Math.max(1,3,4,5,6,7,9,0))
+// console.log(Math.min(1,3,4,5,6,7,9,0))
+// console.log(Number.parseInt (Math.random()*100))
---
+++ index.js
@@ -0,0 +1,86 @@
+// Begin-1 P-?
+// var a = 3
+// console.log(4*a)
+
+// Begin-2 S=?
+// var a = 5
+// console.log(a**2)
+
+// Begin-3 S=?  P=?
+// var a = 2
+// var b = 3
+// console.log(a*b)
+// console.log(2*(a+b))
+
+// Begin-4 L=?
+// var d = 3
+// var p = 3.14
+// console.log(p*d)
+
+// Begin-5 V=?  S=?
+// var a = 4
+// console.log(a**3)
+// console.log(6*a**2)
+
+// Begin-6 V=?  S=?
+// var a = 2
+// var b = 3
+// var c = 4
+// console.log(a*b*c)
+// console.log(2*(a*b+b*c+a*c))
+
+// Begin-7 L=?  S=?
+const p = 3.14
+// var R = 5
+// console.log(2*R*p)
+// console.log(p*R**2)
+
+// Begin-8 O'rta arifmetiik-?
+// var a=2
+// var b=3
+// console.log((a+b)/2)
+
+// Begin-9 O'rta geometrik-?
+// var a=2
+// var b=3
+// console.log((a*b)**0.5)
+
+// Begin-10 
+// var a=4
+// var b=3 
+// console.log(a+b, a*b, a**2, b**2)
+
+// Begin-11 
+// var c=-4
+// var d=3
+// console.log(c+d, c*d, c, d)
+
+// Begin-12 
+// a=2
+// b=3
+// var c= (a**2+b**2)**0.5
+// console.log((a**2+b**2)**0.5)
+// console.log(a+b+c)
+
+    // Begin-13 
+    // var R1=5
+    // var R2=4
+    // var S1 = p*R1
+    // var S2 = p*R2
+    // var S3 = p*(R1-R2)
+    // console.log(S1, S2, S3 )
+
+// Begin-14 
+// var L=5
+// var R = L/2*p
+// var S=p*R**2
+// console.log(R)
+
+// Begin-15
+// var S=5
+// var R = S/2*p
+// var d=p*R**2
+// console.log(d)
+
+// Begin-16
+var 
