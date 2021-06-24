87b9a39692ecdf8c96bb34b17d83322a48b1e9b1
Abdulloh <qiyomovabdulloh3@gmail.com>
2021-06-24 18:18:52 +0500

    js-4

--- index3.js
+++ index3.js
@@ -1,48 +1,35 @@
-
 // First homework
-// var ball = 60
-// if(ball >= 95){
-//     console.log('A+');
-// }else if(ball < 95 && ball >= 90){
-//     console.log('A');
-// }else if(ball < 90 && ball >= 85){
-//     console.log('B+');
-// }else if(ball < 85 && ball >= 80){
-//     console.log('B');
-// }else if(ball < 80 && ball >= 75){
-//     console.log('C+');
-// }else if(ball < 75 && ball >= 70){
-//     console.log('C');
-// }else if(ball < 70 && ball >= 65){
-//     console.log('D');
-// }else if(ball < 65 && ball >= 60){
-//     console.log('F');
-// }else{
-//     console.log('damingni ol')
-// }
+var ball = 60
+if(ball >= 95){
+    console.log('A+');
+}else if(ball < 95 && ball >= 90){
+    console.log('A');
+}else if(ball < 90 && ball >= 85){
+    console.log('B+');
+}else if(ball < 85 && ball >= 80){
+    console.log('B');
+}else if(ball < 80 && ball >= 75){
+    console.log('C+');
+}else if(ball < 75 && ball >= 70){
+    console.log('C');
+}else if(ball < 70 && ball >= 65){
+    console.log('D');
+}else if(ball < 65 && ball >= 60){
+    console.log('F');
+}else{
+    console.log('damingni ol')
+}
 
 // Second 
-var son = '5@59999'
-if(son.includes('@')){
+var son = '3@999'
+if((son.includes('@')) || (son.includes('$'))(son.includes('#'))){
     console.log('Strong password');
 }
 else if(son.length>4 && son.length<8){
-    console.log('The good password');
+    console.log('Password is good!');
+}else{
+    console.log('Wrong Password!')
 }
 
 
-// let string = '30 Days Of JavaScript'
 
-// console.log(string.includes('Days'))     // true
-// console.log(string.includes('days'))     // false - it is case sensitive!
-// console.log(string.includes('Script'))   // true
-// console.log(string.includes('script'))   // false
-// console.log(string.includes('java'))     // false
-// console.log(string.includes('Java'))     // true
-
-// let country = 'Finland'
-
-// console.log(country.includes('fin'))     // false
-// console.log(country.includes('Fin'))     // true
-// console.log(country.includes('land'))    // true
-// console.log(country.includes('Land'))    // false
---
+++ index4.js
@@ -0,0 +1,41 @@
+// First
+// for(let a = 10; a>=0;a--){
+// console.log(a)
+// }
+
+
+// Second
+// for(var a=10;a>=1;a--){
+//     if(a%2!=0)
+//     console.log(a)
+// }
+
+
+// Third
+// for(var a=10;a>1;a--){
+//     if(a%2==0)
+//     console.log(a)
+// }
+
+
+// Fourth
+// for(var a=1;a<100;a++){
+//     if(a%3==0)
+//     console.log(a)
+// }
+
+
+// Fifth
+// for(var a=1;a<100;a++){
+//     if(a%5==0)
+//     console.log(a)
+// }
+
+
+// for(var a=1;a<=10;a++){
+//     if(a%2==0)
+//         console.log(a)
+//     else if(a%2!=0)
+//         console.log(a)
+// }
+
